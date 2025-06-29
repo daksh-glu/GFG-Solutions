352ea023939f693b539c5eb7fd73058e38e2428a
dakshjain2005 <147020461+dakshjain2005@users.noreply.github.com>
2025-06-29 17:22:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Repeated Character/repeated-character.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    char firstRep(string s) {
+        // code here.
+        unordered_map<char, int> freq;
+
+        for (char ch : s) {
+            freq[ch]++;
+        }
+
+        for (char ch : s) {
+            if(freq[ch] > 1){
+                return ch;
+            }
+                
+        }
+
+        return '#'; 
+    }
+};
