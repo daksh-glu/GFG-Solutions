129380d104559024fcbd803fc0337eb503a3647d
Daksh Jain <147020461+daksh-glu@users.noreply.github.com>
2026-03-03 21:32:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest subarray with Atmost two distinct integers/longest-subarray-with-atmost-two-distinct-integers.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int totalElements(vector<int> &arr) {
+        // code here
+        unordered_map<int,int> freq;
+    int left = 0, maxLen = 0;
+    for (int right = 0; right < arr.size(); right++) {
+        freq[arr[right]]++;
+        while (freq.size() > 2) {
+            freq[arr[left]]--;
+            if (freq[arr[left]] == 0) freq.erase(arr[left]);
+            left++;
+        }
+        maxLen = max(maxLen, right - left + 1);
+    }
+    return maxLen;
+    }
+};
