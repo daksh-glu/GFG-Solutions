d31072cc4260d1fbbe83489510d7985e54fddd80
Daksh Jain <147020461+daksh-glu@users.noreply.github.com>
2026-04-26 23:32:40 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.cpp
+++ Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.cpp
@@ -1,31 +1,20 @@
 class Solution {
-public:
-    vector<int> commonElements(vector<int> &arr1, vector<int> &arr2, vector<int> &arr3) {
+  public:
+    vector<int> commonElements(vector<int> &a, vector<int> &b, vector<int> &c) {
+        // code here
+        vector<int> res;
         int i = 0, j = 0, k = 0;
-        vector<int> result;
-
-        while (i < arr1.size() && j < arr2.size() && k < arr3.size()) {
-            if (arr1[i] == arr2[j] && arr2[j] == arr3[k]) {
-                if (result.empty() || result.back() != arr1[i])
-                    result.push_back(arr1[i]);
-                i++;
-                j++;
-                k++;
-            } 
-            else if (arr1[i] < arr2[j]) {
-                i++;
-            } 
-            else if (arr2[j] < arr3[k]) {
-                j++;
-            } 
-            else {
-                k++;
-            }
+        while (i < a.size() && j < b.size() && k < c.size()) {
+            if (a[i] == b[j] && b[j] == c[k]) {
+                res.push_back(a[i]);
+                while (i + 1 < a.size() && a[i] == a[i+1]) i++;
+                while (j + 1 < b.size() && b[j] == b[j+1]) j++;
+                while (k + 1 < c.size() && c[k] == c[k+1]) k++;
+                i++; j++; k++;
+            } else if (a[i] <= b[j] && a[i] <= c[k]) i++;
+            else if (b[j] <= a[i] && b[j] <= c[k]) j++;
+            else k++;
         }
-
-        if (result.empty())
-            return {-1};
-
-        return result;
+        return res;
     }
 };
